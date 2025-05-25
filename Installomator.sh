0afad86a8968f0c76ee12447f8dae0a254874345
Malte Kiefer <malte.kiefer@aleph-alpha.com>
2025-05-25 06:54:33 +0200

    added warp

--- Installomator.sh
+++ Installomator.sh
@@ -10344,6 +10344,13 @@ wallyezflash)
     expectedTeamID="V32BWKSNYH"
     #versionKey="CFBundleVersion"
     ;;
+warp)
+    name="Warp"
+    type="dmg"
+    downloadURL="https://app.warp.dev/download"
+    appNewVersion="$(curl -s https://releases.warp.dev/channel_versions.json | grep -A 3 '"stable"' | grep '"version"' | head -n 1 | sed -E 's/.*"version": *"v([^"]+)".*/\1/')"
+    expectedTeamID="2BBY89MBSN"
+    ;;
 wavescentral)
     name="Waves Central"
     type="dmg"
